5a7c20cd8c9550eedd3fabb5ce9615adf07f3d9d
Paul-Louis NECH <pln@algolia.com>
2017-02-17 18:47:24 +0100

    feat: Use new NumericSelector, logToast nbHits

--- app/src/main/java/com/algolia/instantsearch/mvp/MainActivity.java
+++ app/src/main/java/com/algolia/instantsearch/mvp/MainActivity.java
@@ -14,19 +14,21 @@ import android.widget.PopupWindow;
 import android.widget.Toast;
 
 import com.algolia.instantsearch.events.ErrorEvent;
+import com.algolia.instantsearch.events.ResultEvent;
 import com.algolia.instantsearch.events.SearchEvent;
 import com.algolia.instantsearch.helpers.Searcher;
 import com.algolia.instantsearch.ui.InstantSearchHelper;
-import com.algolia.instantsearch.ui.views.filters.TwoValuesToggle;
 
 import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
+import org.json.JSONException;
 
 public class MainActivity extends AppCompatActivity {
     private static final String ALGOLIA_APP_ID = "latency";
     private static final String ALGOLIA_SEARCH_API_KEY = "3d9875e51fbd20c7754e65422f7ce5e1";
     private static final String ALGOLIA_INDEX_NAME = "bestbuy";
     private PopupWindow mPopupWindow;
+    private InstantSearchHelper helper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,14 +36,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final Searcher searcher = new Searcher(ALGOLIA_APP_ID, ALGOLIA_SEARCH_API_KEY, ALGOLIA_INDEX_NAME);
-        final InstantSearchHelper helper = new InstantSearchHelper(this, searcher);
+        helper = new InstantSearchHelper(this, searcher);
         helper.search(); // First empty search to display default results
+        createPopup();
 
         final Button buttonFilters = (Button) findViewById(R.id.btn_filter);
         buttonFilters.setOnClickListener(new View.OnClickListener() {
             @Override public void onClick(View v) {
                 if (mPopupWindow == null) {
                     createPopup();
+                    showPopup();
                 } else {
                     togglePopup();
                 }
@@ -53,21 +57,6 @@ public class MainActivity extends AppCompatActivity {
                 } else {
                     mPopupWindow.dismiss();
                 }
-            }
-
-            private void createPopup() {
-                // Create ViewGroup by inflating layout
-                View filters = ((LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE)).inflate(R.layout.filters, null);
-
-                // Register all AlgoliaFacetFilters
-                helper.registerFacetFilters((ViewGroup) filters);
-
-                // Create PopupWindow with the filters
-                mPopupWindow = new PopupWindow(filters, LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                    mPopupWindow.setElevation(5.0f);
-                }
-                showPopup();
             }
 
             private void showPopup() {
@@ -97,5 +86,26 @@ public class MainActivity extends AppCompatActivity {
         final String text = "Error:" + event.error.getMessage();
         Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
         Log.e("PLN", text);
+    }
+
+    @Subscribe
+    public void onResult(ResultEvent event) throws JSONException {
+        final String text = "Hits:" + event.content.get("nbHits").toString();
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+        Log.e("PLN", text);
+    }
+
+    private void createPopup() {
+        // Create ViewGroup by inflating layout
+        View filters = ((LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE)).inflate(R.layout.filters, null);
+
+        // Register all AlgoliaFacetFilters
+        helper.registerFacetFilters((ViewGroup) filters);
+
+        // Create PopupWindow with the filters
+        mPopupWindow = new PopupWindow(filters, LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            mPopupWindow.setElevation(5.0f);
+        }
     }
 }
